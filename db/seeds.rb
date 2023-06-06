979e5520d0e6d67af8dbc7317877b2b6d4f9ff1b
Valentine <kerubovalentine1@gmail.com>
2023-06-06 23:07:50 +0300

    create client1

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,11 @@ Project.destroy_all
 
 puts "Seeding projects..."
 
+client1 = Project.create(
+  title: "Client 1",
+  favorite: false,
+	#description: "Client 1's project",
+  color: '#e7aaa2'
+)
+
 puts "✅ Done seeding!"
