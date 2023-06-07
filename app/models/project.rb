bc0b7fa119eb87e3f9557d50ddeb7dc8e577e95b
Valentine <kerubovalentine1@gmail.com>
2023-06-07 12:10:22 +0300

    define project relationships

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,6 +2,9 @@ require_relative './models_module'
 
 class Project < ActiveRecord::Base
     extend InheritMethods::ClassMethods # 'extend' is used to define class methods
+    has_many :boards, dependent: :destroy
+    has_many :tasks, through: :boards
+
 
 
 end
