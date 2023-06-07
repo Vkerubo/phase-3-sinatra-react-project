f9273c29b0c76f9f0b06c41b8aca7f11e7bc0fbc
Valentine <kerubovalentine1@gmail.com>
2023-06-07 12:19:45 +0300

    render tasks formatted

--- app/models/task.rb
+++ app/models/task.rb
@@ -6,5 +6,26 @@ class Task < ActiveRecord::Base
     belongs_to :board
     has_one :project, through: :board
 
+    # Class method to render all tasks formatted for the frontend
+  def self.render_all_formatted_for_frontend
+    def self.render_all_formatted_for_frontend
+        self.all.map do |task|
+            {
+            id: task.id,
+            name: task.name,
+            due_date: task.due_date,
+            description: task.description,
+            status: task.status,
+            priority: task.priority,
+            completed: task.completed,
+            board: task.board.name,
+            board_id: task.board_id,
+            project: task.board.project.title,
+            project_id: task.board.project_id,
+            created_at: task.created_at,
+            updated_at: task.updated_at
+            }
+        end
+    end
 
 end
