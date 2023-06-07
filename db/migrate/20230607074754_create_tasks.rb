11026e25d49c75be155cf2adc0359851731b3d16
Valentine <kerubovalentine1@gmail.com>
2023-06-07 11:24:11 +0300

    create tasks table

--- db/migrate/20230607074754_create_tasks.rb
+++ db/migrate/20230607074754_create_tasks.rb
@@ -1,4 +1,13 @@
 class CreateTasks < ActiveRecord::Migration[6.1]
   def change
+     create_table :tasks do |t|
+      t.string :name
+      t.date :due_date
+      t.text :description
+      t.string :status
+      t.string :priority
+      t.boolean :completed
+
+    end
   end
 end
