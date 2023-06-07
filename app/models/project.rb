6905fa75025679e34fc75a60e584b4d4af0a6c64
Valentine <kerubovalentine1@gmail.com>
2023-06-07 12:13:20 +0300

    create new project with default values

--- app/models/project.rb
+++ app/models/project.rb
@@ -5,6 +5,12 @@ class Project < ActiveRecord::Base
     has_many :boards, dependent: :destroy
     has_many :tasks, through: :boards
 
+    # Class method to create a new project with default values
+    def self.create_new_project_with_defaults(hash)
+        title = hash["title"] ? hash["title"] : "New Project" # Assigning a default title if not provided
+        favorite = hash["favorite"] ? hash["favorite"] : false # Assigning a default favorite status if not provided
+        color = hash["color"] ? hash["color"] : '#e1bee7'  # Assigning a default color if not provided
+
 
 
 end
