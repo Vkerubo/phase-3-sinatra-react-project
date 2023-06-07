77881dbcc06ebb46437fbef2ea970a5a6f4348b3
Valentine <kerubovalentine1@gmail.com>
2023-06-07 12:18:45 +0300

    define Task relationships

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,5 +3,8 @@ require_relative './models_module'
 class Task < ActiveRecord::Base
     extend InheritMethods::ClassMethods # 'extend' is used to define class methods
 
+    belongs_to :board
+    has_one :project, through: :board
+
 
 end
