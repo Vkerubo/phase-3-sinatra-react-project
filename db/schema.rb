dd4ef43e13a7185d8c323b2c3080675803c93059
Valentine <kerubovalentine1@gmail.com>
2023-06-07 15:04:29 +0300

    add favorite column on schema.rb: projects

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_06_07_080055) do
+ActiveRecord::Schema.define(version: 2023_06_07_115954) do
 
   create_table "boards", force: :cascade do |t|
     t.string "name"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2023_06_07_080055) do
     t.string "title"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "favorite"
   end
 
   create_table "tasks", force: :cascade do |t|
